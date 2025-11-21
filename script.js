842957e9990e7d7eb7c387e2359c8b911ff19f80
aniketgadekar04 <156211209+aniketgadekar04@users.noreply.github.com>
2025-11-21 21:50:01 +0530

    basic js added

---
+++ script.js
@@ -0,0 +1,27 @@
+let targetColor = "";
+let time = 30;
+let score = 0;
+let timer;
+let colors = [
+  "red",
+  "orange",
+  "blue",
+  "green",
+  "black",
+  "gray",
+  "hotpink",
+  "purple",
+  "white",
+  "yellow",
+  "lightgreen",
+  "maroon",
+  "khaki",
+  "brown",
+  "navy",
+  "indigo",
+];
+
+function startGame() {
+  let time = 30;
+  let score = 0;
+}
