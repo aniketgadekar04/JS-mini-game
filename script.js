68353f7fc4a6d75343f4c332e6dd40b06646ecce
aniketgadekar04 <156211209+aniketgadekar04@users.noreply.github.com>
2025-11-22 01:16:49 +0530

    time and score displayed

--- script.js
+++ script.js
@@ -21,7 +21,22 @@ let colors = [
   "indigo",
 ];
 
+const grid = document.getElementById("grid")
+const targetColorDisplay = document.getElementById('target-color')
+const scoreDisplay = document.getElementById('score')
+const timeDisplay = document.getElementById('time')
+
+// function for random color
+
+
+
 function startGame() {
   let time = 30;
   let score = 0;
+
+  scoreDisplay.textContent = score
+  timeDisplay.textContent = time
+  createGrid()
+
+  
 }
